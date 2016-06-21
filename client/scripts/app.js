e1ec83cb81e1564f03dbc8d6d6ad8e14b26a5b5d
vinnyhuang <vincent.vwh.huang@gmail.com>
2016-06-20 19:43:24 -0700

    Added functionality to clear and add messages with respect to DOM, add rooms to DOM, add friends, and submit messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,8 @@
 
 var app = {
   server: 'https://api.parse.com/1/classes/messages',
+  myUser: undefined,
+  users: {},
   init() {},
   send(message) {
     /*var message = {
@@ -24,19 +26,69 @@ var app = {
       }
     });
   },
+
   fetch() {
     $.ajax({
       url: this.server,
       type: 'GET',
       success: function (data) {
         console.log('chatterbox: Message fetched', data);
+        app.clearMessages();
+        for (var i = data.results.length - 1; i >= 0; i--) {
+          app.addMessage(data.results[i]);
+        }
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to fetch message', data);
       }
     });
+  },
+
+  clearMessages() {
+    $('#chats').children().remove();
+  },
+
+  addMessage(message) {
+    var $msg = `<div class="message"><h3 class="username">${message.username}:</h3><p class="text">${message.text}</p></div>`;
+    $('#chats').prepend($msg);
+    if (this.users[message.username] === undefined) {
+      this.users[message.username] = false;
+    }
+    $('#chats').children().first().click((event) => {
+      this.addFriend(message.username);
+    });
+  },
+
+  addRoom(roomName) {
+    var $room = `<option>${roomName}</option>`;
+    $('#roomSelect').append($room);
+  },
+
+  addFriend(username) {
+    this.users[username] = true;
+  },
+
+  handleSubmit() {
+    //event.preventDefault();
+    var message = {
+      username: app.myUser,
+      text: $('#message').val(),
+      roomname: 'chatroom'
+    };
+    console.log("here");
+    //console.log($('#userInput'));
+    app.send(message);
+    app.fetch();
   }
 };
 
-app.fetch();
+$(document).ready( () => {
+  app.fetch();
+  app.myUser = window.location.search.match(/username=(.+)/)[1];
+  $('form').submit((event) => { 
+    app.handleSubmit();
+    event.preventDefault();
+  });
+});
+
